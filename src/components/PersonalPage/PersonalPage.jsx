b948154bce8d48c8c26f45be467721fddd7dcd0c
Y-Negash <n.yeabsira.a@gmail.com>
2024-08-02 13:26:00 +0100

    adds a default profile pic and placeholder links array.

--- src/components/PersonalPage/PersonalPage.jsx
+++ src/components/PersonalPage/PersonalPage.jsx
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
+import "./PersonalPage.css";
 
 const PersonalPage = () => {
   let { userId } = useParams();
+  const links = [
+    "https://www.linkedin.com/feed/",
+    "https://github.com/",
+    "https://www.youtube.com/",
+  ];
+
   return (
     <div>
       <h1>{userId}&apos;s Personal Page</h1>
+      <img
+        className="profile-pic"
+        src="https://cdn.pixabay.com/photo/2017/06/13/12/54/profile-2398783_1280.png"
+      />
     </div>
   );
 };
