cd88901085de186cd571376b8c7a846fd4a2d10e
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-03 16:12:57 +0100

    Install prop-types package. Add proptypes to UserCard component.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,22 +3,21 @@ import UserCard from "./UserCard/UserCard";
 const Home = () => {
   let userList = [];
   for (let i = 0; i <= 10; i++) {
-    const user =
-    {
+    const user = {
       name: `user${i}`,
       "profile-pic": "either from asset folder or online",
       type: "pokemon type",
       occupation: "",
       description: "description less than 50 chars.",
       links: ["link 1", "link 2", "link 3"],
-      skills: [ "skill 1", "skill 2", "skill3"],  // max 3 skills
-    }      
+      skills: ["skill 1", "skill 2", "skill3"], // max 3 skills
+    };
     userList.push(user);
   }
   const userCardComponents = userList.map((user, i) => {
-    return <UserCard user={user} key={`user${i}`} />
-  })
-  
+    return <UserCard user={user} key={`user${i}`} />;
+  });
+
   return (
     <div>
       <h1>Home</h1>
