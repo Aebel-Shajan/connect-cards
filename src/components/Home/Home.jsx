3baec93ba90ed6e3f2a8e99cb6549859482862b4
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-03 20:27:33 +0100

    Update test user data.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,8 +8,12 @@ const Home = () => {
       image: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png",
       type: "pokemon type",
       occupation: "occupation",
-      description: "description less than 50 chars.",
-      links: ["link 1", "link 2", "link 3"],
+      description: "Hello this is a description which is very long and it will continue more and more and more and more. This bit however will not be displayed",
+      links: {
+        "LinkedIn": "LinkedIn.com",
+        "Github": "Github.com",
+        "Portfolio": "portfolio.com"
+      },
       skills: ["skill 1", "skill 2", "skill3"], // max 3 skills
     };
     userList.push(user);
