58c53a7c1862cccc4e6998f38f529fdf47e372bc
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-03 21:18:32 +0100

    Add link section to UserCard component.

--- src/components/Home/UserCard/UserCard.jsx
+++ src/components/Home/UserCard/UserCard.jsx
@@ -1,7 +1,39 @@
 import PropTypes from "prop-types";
+import { FaHackerrank, FaGithub, FaLinkedin, FaInstagram, FaFacebook, FaDiscord, FaLink  } from "react-icons/fa";
+import { CgPokemon } from "react-icons/cg";
 import "./UserCard.css";
 
 const UserCard = ({ user }) => {
+
+  let linkComponents = [];
+  const iconReference = {
+    "linkedin": <FaLinkedin />,
+    "github": <FaGithub />,
+    "hackerrank": <FaHackerrank />,
+    "instagram": <FaInstagram />,
+    "facebook": <FaFacebook />,
+    "discord": <FaDiscord />,
+    "pokemon": <CgPokemon />
+  }
+  for ( let i = 0; i< 3; i++) {
+    const linkName = Object.keys(user.links)[i]
+    let linkLogo = <FaLink />
+    if (Object.keys(iconReference).includes(linkName.toLowerCase())) {
+      linkLogo = iconReference[linkName.toLowerCase()];
+    }
+    linkComponents[i] = (
+      <a 
+        className="card-link"
+        href={user.links[linkName]}
+        key={`link${i}`}
+        >
+          {linkLogo}
+          {linkName}
+      </a>
+    )
+  }
+
+
   return (
     <div className="card-container">
       <div className="card-header-container">
@@ -22,7 +54,7 @@ const UserCard = ({ user }) => {
         {user.description.substring(0, 100)}
       </div>
       <div className="card-link-container">
-        {}
+        {linkComponents}
       </div>
       <div className="card-skill-container">
 
@@ -38,7 +70,7 @@ UserCard.propTypes = {
     type: PropTypes.string,
     occupation: PropTypes.string,
     description: PropTypes.string,
-    links: PropTypes.arrayOf(PropTypes.string),
+    links: PropTypes.object,
     skills: PropTypes.arrayOf(PropTypes.string),
   }),
 };
