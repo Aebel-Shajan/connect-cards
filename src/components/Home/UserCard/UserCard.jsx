4615c31d188174ed22a08a6ee8ddaa9a5a578042
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-05 22:00:44 +0100

    Refactor skills and links sections in UserCard component.

--- src/components/Home/UserCard/UserCard.jsx
+++ src/components/Home/UserCard/UserCard.jsx
@@ -1,39 +1,10 @@
 import PropTypes from "prop-types";
-import { FaHackerrank, FaGithub, FaLinkedin, FaInstagram, FaFacebook, FaDiscord, FaLink  } from "react-icons/fa";
+import { FaHackerrank, FaGithub, FaLinkedin, FaInstagram, FaFacebook, FaDiscord, FaLink } from "react-icons/fa";
 import { CgPokemon } from "react-icons/cg";
 import "./UserCard.css";
+import markdownBadges from "../../../assets/markdown-badges.json"
 
 const UserCard = ({ user }) => {
-
-  let linkComponents = [];
-  const iconReference = {
-    "linkedin": <FaLinkedin />,
-    "github": <FaGithub />,
-    "hackerrank": <FaHackerrank />,
-    "instagram": <FaInstagram />,
-    "facebook": <FaFacebook />,
-    "discord": <FaDiscord />,
-    "pokemon": <CgPokemon />
-  }
-  for ( let i = 0; i< 3; i++) {
-    const linkName = Object.keys(user.links)[i]
-    let linkLogo = <FaLink />
-    if (Object.keys(iconReference).includes(linkName.toLowerCase())) {
-      linkLogo = iconReference[linkName.toLowerCase()];
-    }
-    linkComponents[i] = (
-      <a 
-        className="card-link"
-        href={user.links[linkName]}
-        key={`link${i}`}
-        >
-          {linkLogo}
-          {linkName}
-      </a>
-    )
-  }
-
-
   return (
     <div className="card-container">
       <div className="card-header-container">
@@ -53,17 +24,81 @@ const UserCard = ({ user }) => {
       <div className="card-description">
         {user.description.substring(0, 100)}
       </div>
-      <div className="card-link-container">
-        {linkComponents}
-      </div>
-      <div className="card-skill-container">
-        {user.skills.slice(0, 3).map((skill, index) => {
-          return <div className="card-skill" key={`user ${user.name} skill${index}`}>{skill}</div>
-        })}
-      </div>
+      <UserLinks links={user.links} />
+      <UserSkills skills={user.skills} />
     </div>
   );
 };
+
+
+const UserLinks = ({ links }) => {
+  let linkComponents = [];
+  const iconReference = {
+    "linkedin": <FaLinkedin />,
+    "github": <FaGithub />,
+    "hackerrank": <FaHackerrank />,
+    "instagram": <FaInstagram />,
+    "facebook": <FaFacebook />,
+    "discord": <FaDiscord />,
+    "pokemon": <CgPokemon />
+  }
+
+  for (let i = 0; i < 3; i++) {
+    const linkName = Object.keys(links)[i]
+    let linkLogo = <FaLink />
+    if (Object.keys(iconReference).includes(linkName.toLowerCase())) {
+      linkLogo = iconReference[linkName.toLowerCase()];
+    }
+    linkComponents[i] = (
+      <a
+        className="card-link"
+        href={links[linkName]}
+        key={`link${i}`}
+      >
+        {linkLogo}
+        {linkName}
+      </a>
+    )
+  }
+
+  return (
+    <div className="card-link-container">
+      {linkComponents}
+    </div>
+  )
+}
+
+
+const UserSkills = ({ skills }) => {
+  let skillComponents = [];
+  for (let i = 0; i < 3; i++) {
+    const skillName = skills[i]
+    let skillBadge = (
+      <div 
+        className="card-skill" 
+        key={`user skill${i}`}>
+        {skillName}
+      </div>
+    )
+    if (Object.keys(markdownBadges).includes(skillName)) {
+      skillBadge = (
+        <div
+          className="card-skill"
+          key={`user skill${i}`}>
+          <div className="card-skill-overlay"></div>
+          <img src={markdownBadges[skillName]} />
+        </div>
+      )
+    }
+    skillComponents[i] = skillBadge 
+  }
+
+  return (
+  <div className="card-skill-container">
+    {skillComponents}
+  </div>
+  )
+}
 
 UserCard.propTypes = {
   user: PropTypes.shape({
@@ -76,5 +111,13 @@ UserCard.propTypes = {
     skills: PropTypes.arrayOf(PropTypes.string),
   }),
 };
+
+UserLinks.propTypes = {
+  links: PropTypes.object,
+}
+
+UserSkills.propTypes = {
+  skills: PropTypes.arrayOf(PropTypes.string)
+}
 
 export default UserCard;
