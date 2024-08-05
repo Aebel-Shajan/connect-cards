04e549f12d9c4680ef30612f97e06954e97ddef2
kajanan <kajanan15i@gmail.com>
2024-08-05 22:47:57 +0100

    slightly modified the 'home' component to display users from the json file

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,20 +3,7 @@ import UserCard from "./UserCard/UserCard";
 
 const Home = ( { users, setSearchValue } ) => {
 
-  let userList = [];
-  for (let i = 0; i <= 10; i++) {
-    const user = {
-      name: `user${i}`,
-      "profile-pic": "either from asset folder or online",
-      type: "pokemon type",
-      occupation: "",
-      description: "description less than 50 chars.",
-      links: ["link 1", "link 2", "link 3"],
-      skills: ["skill 1", "skill 2", "skill3"], // max 3 skills
-    };
-    userList.push(user);
-  }
-  const userCardComponents = userList.map((user, i) => {
+  const userCardComponents = users.map((user, i) => {
     return <UserCard user={user} key={`user${i}`} />;
   });
 
