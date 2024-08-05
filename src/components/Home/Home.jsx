c1eb4733be3d0d7f7e3b6fb6d30c27b192ea2291
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-06 00:00:21 +0100

    Format code, oops i forgot to do this

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,14 +5,16 @@ const Home = () => {
   for (let i = 0; i <= 10; i++) {
     const user = {
       name: `Pikachu ${i}`,
-      image: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png",
+      image:
+        "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/025.png",
       type: "data",
       occupation: "Data Engineer",
-      description: "Hello this is a description which is very long and it will continue more and more and more and more. This bit however will not be displayed",
+      description:
+        "Hello this is a description which is very long and it will continue more and more and more and more. This bit however will not be displayed",
       links: {
-        "LinkedIn": "LinkedIn.com",
-        "Github": "Github.com",
-        "Portfolio": "portfolio.com"
+        LinkedIn: "LinkedIn.com",
+        Github: "Github.com",
+        Portfolio: "portfolio.com",
       },
       skills: ["Apache Spark", "Cloudera", "3DS"], // max 3 skills
     };
--- src/components/Home/UserCard/UserCard.jsx
+++ src/components/Home/UserCard/UserCard.jsx
@@ -2,71 +2,73 @@ import PropTypes from "prop-types";
 import "./UserCard.css";
 import UserSkills from "./UserSkills/UserSkills";
 import UserLinks from "./UserLinks/UserLinks";
-import { FaBrain, FaCloud, FaDatabase, FaInfinity, FaReact, FaRegDotCircle, FaShieldAlt } from "react-icons/fa";
+import {
+  FaBrain,
+  FaCloud,
+  FaDatabase,
+  FaInfinity,
+  FaReact,
+  FaRegDotCircle,
+  FaShieldAlt,
+} from "react-icons/fa";
 import { FaGear } from "react-icons/fa6";
 
 const UserCard = ({ user }) => {
   const typeReference = {
-    "data": {
-      "icon": <FaDatabase />,
-      "color": "#4DB8FF" // light blue
+    data: {
+      icon: <FaDatabase />,
+      color: "#4DB8FF", // light blue
     },
-    "backend": {
-      "icon": <FaGear />,
-      "color": "#FF8C00" // dark orange
+    backend: {
+      icon: <FaGear />,
+      color: "#FF8C00", // dark orange
     },
-    "frontend": {
-      "icon": <FaReact />,
-      "color": "#61DAFB" // react blue
+    frontend: {
+      icon: <FaReact />,
+      color: "#61DAFB", // react blue
     },
-    "devops": {
-      "icon": <FaInfinity />,
-      "color": "#FF4500" // orange-red
+    devops: {
+      icon: <FaInfinity />,
+      color: "#FF4500", // orange-red
     },
-    "cybersecurity": {
-      "icon": <FaShieldAlt />,
-      "color": "#32CD32" // lime green
+    cybersecurity: {
+      icon: <FaShieldAlt />,
+      color: "#32CD32", // lime green
     },
-    "cloud": {
-      "icon": <FaCloud />,
-      "color": "#1E90FF" // dodger blue
+    cloud: {
+      icon: <FaCloud />,
+      color: "#1E90FF", // dodger blue
     },
-    "ai": {
-      "icon": <FaBrain />,
-      "color": "#FF1493" // deep pink
+    ai: {
+      icon: <FaBrain />,
+      color: "#FF1493", // deep pink
     },
-    "normal": {
-      "icon": <FaRegDotCircle />,
-      "color": "#d3d3d3" // light grey
+    normal: {
+      icon: <FaRegDotCircle />,
+      color: "#d3d3d3", // light grey
     },
   };
 
-  let typeStyle = typeReference["normal"]
+  let typeStyle = typeReference["normal"];
   if (Object.keys(typeReference).includes(user.type.toLowerCase())) {
     typeStyle = typeReference[user.type];
   }
-
 
   return (
     <div
       className="card-container"
       style={{
-        "--type-color-transparent": typeStyle["color"] + "88"
-        }}>
+        "--type-color-transparent": typeStyle["color"] + "88",
+      }}
+    >
       <div className="card-header-container">
-        <div className="card-title">
-          {user.name}
-        </div>
-        <div className="card-type">
-          {typeStyle["icon"]}
-        </div>
+        <div className="card-title">{user.name}</div>
+        <div className="card-type">{typeStyle["icon"]}</div>
       </div>
       <div className="card-image-container">
         <img className="card-image" src={user.image} />
       </div>
-      <div className="card-occupation">
-        {user.occupation}
-      </div>
+      <div className="card-occupation">{user.occupation}</div>
       <div className="card-description">
         {user.description.substring(0, 100)}
       </div>
