bfba68944a111d5fbdac5aea5aa7bbabad610044
kajanan <kajanan15i@gmail.com>
2024-08-09 11:59:59 +0100

    Filtering feature with list of common widely used tech skills

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,15 +3,29 @@ import UserCard from "./UserCard/UserCard";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import "./Home.css";
+import SkillsFilter from "./Filter/SkillsFilter";
 
 const Home = ({ users }) => {
+
   const [searchValue, setSearchValue] = useState("");
+  const [filterValue, setFilterValue] = useState("");
+
   const navigate = useNavigate();
 
+
   const filteredUserCards = users.filter((user) => {
-    if (searchValue) {
-      return user.name.toLowerCase().startsWith(searchValue.toLowerCase());
+    if (searchValue && filterValue) {
+      return user.name.toLowerCase().startsWith(searchValue.toLowerCase()) && user.skills.includes(filterValue);
     }
+
+    if(searchValue) {
+      return user.name.toLowerCase().startsWith(searchValue.toLowerCase())
+    }
+
+    if(filterValue) {
+      return user.skills.includes(filterValue);
+    }
+
     return true;
   });
 
@@ -29,6 +43,7 @@ const Home = ({ users }) => {
     <div className="home-container">
       <h1>Home</h1>
       <Search setSearchValue={setSearchValue} />
+      <SkillsFilter setFilterValue={setFilterValue} />
       <div className="user-card-container">{userCardComponents}</div>
     </div>
   );
