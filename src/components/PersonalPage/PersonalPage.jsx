95c658acaa77a8f28e184b47cbdaeea43248067a
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-09 21:31:18 +0100

    Modify personal page component to show just card. Add buterfree background.

--- src/components/PersonalPage/PersonalPage.jsx
+++ src/components/PersonalPage/PersonalPage.jsx
@@ -2,33 +2,23 @@ import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
 import "./PersonalPage.css";
 import {} from "module";
-import profileIcon from "../../assets/profile-icon.png";
-import UserLinks from "./Links/UserLinks";
+import UserCard from "../Home/UserCard/UserCard";
 
 const PersonalPage = ({ users }) => {
   let { userId } = useParams();
   const user = users.find((user) => user.name === userId);
   console.log(users);
-  const links = user?.links;
 
   return (
-    <main className="body">
-      <div className="header">
-        <img className="profile-pic" src={profileIcon} />
-        <h1 id="title">{userId}&apos;s Personal Page</h1>
-      </div>
-      <div>
-        <UserLinks links={links} />
-      </div>
-    </main>
+    <div className="personal-page-container">
+      <div className="background"></div>
+      <UserCard user={user} />
+    </div>
   );
 };
 
 PersonalPage.propTypes = {
-  user: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    links: PropTypes.arrayOf(PropTypes.object).isRequired,
-  }),
+  users: PropTypes.array
 };
 
 export default PersonalPage;
