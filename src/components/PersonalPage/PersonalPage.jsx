6f72b10684aaa576fffe8e96f25373791db2c8be
Y-Negash <n.yeabsira.a@gmail.com>
2024-08-12 14:00:42 +0100

    adds back button to personal page and styles it

--- src/components/PersonalPage/PersonalPage.jsx
+++ src/components/PersonalPage/PersonalPage.jsx
@@ -17,12 +17,19 @@ const PersonalPage = ({ users }) => {
     return <NotFoundPage />;
   }
 
+  const handleButton = () => {
+    window.history.back();
+  }
+
   const userName = user.name[0].toUpperCase() + user.name.substring(1);
   const links = user?.links;
   console.log(user);
   return (
     <div className="container">
       <header>
+      <div className="button-container">
+        <button id="back-button" onClick={handleButton}>Back</button>
+      </div>
         <img className="profile-pic" src={profileIcon} />
         <h1 id="title">{userName}&apos;s Personal Page</h1>
       </header>
