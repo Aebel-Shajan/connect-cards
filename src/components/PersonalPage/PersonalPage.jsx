ce818287ec4ab435d84c894cf908953999deadab
Y-Negash <n.yeabsira.a@gmail.com>
2024-08-13 10:28:49 +0100

    another fix for the mobile format with links as card

--- src/components/PersonalPage/PersonalPage.jsx
+++ src/components/PersonalPage/PersonalPage.jsx
@@ -19,7 +19,7 @@ const PersonalPage = ({ users }) => {
 
   const handleButton = () => {
     window.history.back();
-  }
+  };
 
   const userName = user.name[0].toUpperCase() + user.name.substring(1);
   const links = user?.links;
@@ -27,9 +27,11 @@ const PersonalPage = ({ users }) => {
   return (
     <div className="container">
       <header>
-      <div className="button-container">
-        <button id="back-button" onClick={handleButton}>Back</button>
-      </div>
+        <div className="button-container">
+          <button id="back-button" onClick={handleButton}>
+            Back
+          </button>
+        </div>
         <h1 id="title">{userName}&apos;s Personal Page</h1>
       </header>
       <div className="user-info-container">
