83eeb851e74e556ba24817310069c5865e06c075
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-18 20:24:29 +0100

    Refactor about us page.

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -4,13 +4,12 @@ import Navigation from "../../components/Navigation/Navigation";
 
 const AboutUsPage = () => {
   return (
-    <>
       <div className="about-us-container">
         <Navigation />
-        <h1 id="about-title">About Us!</h1>
-        <div className="app-desc-container">
-          <h3 className="title1">What is Connect Cards?</h3>
-          <p className="paragraph1">
+        <div className="description-container">
+          <h1 >About Us!</h1>
+          <h3>What is Connect Cards?</h3>
+          <p>
             This app is designed to help you connect with a diverse and unique
             network of individuals. Whether youre looking to make new friends,
             find collaborators for projects, or expand your professional
@@ -19,36 +18,26 @@ const AboutUsPage = () => {
             skills, showcase your work, and discover opportunities to
             collaborate with others who share your passions.
           </p>
-        </div>
 
-        <div className="participation-desc-container">
-          <h3 className="title2">How You Can Be Part of It!</h3>
-          <p className="paragraph2">
+          <h3>How You Can Be Part of It!</h3>
+          <p>
             Want to contribute to this project and become part of our growing
             network? It&rsquo;s easy! Join our GitHub repository and add your
             own profile to the JSON file. By doing so, you&rsquo;ll be able to
             share your background, skills, and interests with the community.
           </p>
-          <p className="paragraph3">
+          <p>
             Here&rsquo;s the format to use when adding your profile:
           </p>
 
-          <div className="json-img-container">
-            <img
-              className="json-img"
-              src={jsonpic}
-              alt="JSON example code"
-            ></img>
-          </div>
 
-          <p className="paragraph4">
+          <p>
             By adding your profile, you&rsquo;ll make it easier for others to
             connect with you and explore what you bring to the table. Join us
             today and become a vital part of this innovative project!
           </p>
         </div>
       </div>
-    </>
   );
 };
 
