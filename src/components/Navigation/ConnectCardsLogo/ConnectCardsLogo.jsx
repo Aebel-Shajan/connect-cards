77b159040d3a66f4601ba5d0e71f5ccbe7a85245
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-18 02:50:48 +0100

    Modify how options are obtained. Format code.

--- src/components/Navigation/ConnectCardsLogo/ConnectCardsLogo.jsx
+++ src/components/Navigation/ConnectCardsLogo/ConnectCardsLogo.jsx
@@ -1,18 +1,16 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "./ConnectCardsLogo.css";
 
-const ConnectCardsLogo = ({onClick}) => {
-    return ( 
-        <div 
-            className="logo-container"
-            onClick={onClick}>
-            Connect Cards
-        </div>
-     );
-}
+const ConnectCardsLogo = ({ onClick }) => {
+  return (
+    <div className="logo-container" onClick={onClick}>
+      Connect Cards
+    </div>
+  );
+};
 
 ConnectCardsLogo.propTypes = {
-    onClick: PropTypes.func
+  onClick: PropTypes.func,
 };
- 
+
 export default ConnectCardsLogo;
