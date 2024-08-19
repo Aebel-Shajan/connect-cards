7a94c495bb9d0ded61badf541760751c9cc1f1ff
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-08-19 22:20:56 +0100

    Create TextContainer component.

---
+++ src/components/TextContainer/TextContainer.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import "./TextContainer.css";
+
+const TextContainer = ({ children, className }) => {
+    return (
+        <div className={`text-container ${className}`} >
+            {children}
+        </div>
+    );
+};
+
+TextContainer.propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string
+};
+
+export default TextContainer;
